2d9eeaa13ebc27dc87a03ef7101835a4a5d4acd7
Zuhanit <zuhanit3@gmail.com>
2025-03-20 08:24:08 +0900

    feat: Add terrain model

---
+++ backend/app/models/terrain.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel
+from typing import NamedTuple, TypeAlias, Literal
+
+class Size(NamedTuple):
+  height: int
+  width: int
+
+class Tile(NamedTuple):
+  group: int
+  id: int
+
+Tileset: TypeAlias = Literal[
+  "Ashworld",
+  "Badlands",
+  "Desert",
+  "Ice",
+  "Installation",
+  "Jungle",
+  "Platform",
+  "Twilight"
+]
+
+EraTilesetDict: dict[int, Tileset] = {
+  0: "Ashworld",
+  1: "Badlands",
+  2: "Desert",
+  3: "Ice",
+  4: "Installation",
+  5: "Jungle",
+  6: "Platform",
+  7: "Twilight"
+}
+
+class RawTerrain(BaseModel):
+  """Raw terrain model.
+  
+  `RawTerrain` only have non converted tile data(e.g. tile image), because chk doesn't need
+  to know how tile renders.
+  """
+  size: Size
+  tileset: Tileset
+  tile_id: list[list[Tile]]
+
+class Terrain(BaseModel):
+  size: Size
+  tileset: Tileset
+  tile_id: list[list[Tile]]
+  
