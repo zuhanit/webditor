b4ec6f60fd4333623226b779667f62faf7edcbe9
Zuhanit <zuhanit3@gmail.com>
2025-03-24 08:56:27 +0900

    fix: Use IntFlag

--- backend/app/models/sprite.py
+++ backend/app/models/sprite.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
 from app.models.spatial import Position2D
-from enum import Flag
+from enum import IntFlag 
 
-class SpriteFlag(Flag):
+class SpriteFlag(IntFlag):
   DRAW_AS_SPRITE = 0b0001000000000000  # Bit 12 (4096, 0x1000)
   DISABLED = 0b1000000000000000  # Bit 15 (32768, 0x8000)
 
