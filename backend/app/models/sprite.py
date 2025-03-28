6ac1404e2b1134e776b9b65b16ec80fddccd1cf9
Zuhanit <zuhanit3@gmail.com>
2025-03-29 01:22:54 +0900

    chore: ruff formatting

--- backend/app/models/sprite.py
+++ backend/app/models/sprite.py
@@ -7,6 +7,7 @@ class SpriteFlag(IntFlag):
   DRAW_AS_SPRITE = 0b0001000000000000  # Bit 12 (4096, 0x1000)
   DISABLED = 0b1000000000000000  # Bit 15 (32768, 0x8000)
 
+
 class Sprite(BaseModel):
   id: int
   position: Position2D
