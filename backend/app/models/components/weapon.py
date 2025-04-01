87c3997cfa12018312c9384b0dfc811f82977bdc
Zuhanit <zuhanit3@gmail.com>
2025-04-01 19:11:42 +0900

    feat: Damage member

--- backend/app/models/components/weapon.py
+++ backend/app/models/components/weapon.py
@@ -1,5 +1,8 @@
+from pydantic import Field
 from .component import EntityComponent
 
 
 class Weapon(EntityComponent):
+  base_damage: int = Field(default=0)
+  damage_factor: int = Field(default=0)
   pass
