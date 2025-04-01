0ca0a5101845269339918bcb5d4fdc1428b0cf19
Zuhanit <zuhanit3@gmail.com>
2025-04-01 19:16:25 +0900

    fix: python plain bytes to json validator

---
+++ backend/app/services/hex_validator.py
@@ -0,0 +1,15 @@
+from pydantic import PlainValidator, PlainSerializer, errors, WithJsonSchema
+from typing import Any
+from typing_extensions import Annotated
+
+def hex_bytes_validator(o: Any) -> bytes:
+    if isinstance(o, bytes):
+        return o
+    elif isinstance(o, bytearray):
+        return bytes(o)
+    elif isinstance(o, str):
+        return bytes.fromhex(o)
+    raise errors.BytesError()
+
+
+HexBytes = Annotated[bytes, PlainValidator(hex_bytes_validator), PlainSerializer(lambda b: b.hex()), WithJsonSchema({'type': 'string'})]
