8952821218ee4417fb541a459613b1d59ad3fce9
Zuhanit <zuhanit3@gmail.com>
2025-04-08 02:13:10 +0900

    feat: dat file parsing

---
+++ backend/app/services/rawdata/dat/__init__.py
@@ -0,0 +1,25 @@
+from .scdat import (
+  FlingyDat,
+  ImagesDat,
+  OrdersDat,
+  PortdataDat,
+  SfxdataDat,
+  SpritesDat,
+  TechdataDat,
+  UnitsDat,
+  UpgradesDat,
+  WeaponsDat
+)
+
+__all__ = [
+  "FlingyDat",
+  "ImagesDat",
+  "OrdersDat",
+  "PortdataDat",
+  "SfxdataDat",
+  "SpritesDat",
+  "TechdataDat",
+  "UnitsDat",
+  "UpgradesDat",
+  "WeaponsDat",
+]
