b0ea90e0ecefe307b2fa45d16fff1f2df72c8013
Zuhanit <zuhanit3@gmail.com>
2025-04-30 20:27:24 +0900

    fix: into backend/app/static

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")"
 cd ../preprocess
 poetry run python -m terrain.main
-poetry run python -m graphics.main --path ./graphics/anim --output ../backend/static/anim --sd --hd
+poetry run python -m graphics.main --path ./graphics/anim --output ../backend/app/static/anim --sd --hd
