070a5337cf1756c500efa2f6ac785052d8a0017a
Zuhanit <zuhanit3@gmail.com>
2025-04-30 20:37:30 +0900

    fix: mount static folder

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app.api.v1.endpoints import map, user, tileset
 from app.core.firebase import config  # Automatically initialized by importing `config`.
 from app.core.w_logging import get_logger, setup_logging
@@ -24,23 +25,26 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 logger = get_logger("fastapi")
+
 
 @app.middleware("http")
 async def log_reqeust(request: Request, call_next):
   logger.info(f"Request: {request.method} {request.url}")
-  
+
   user_id = "anonymous"
   auth_header = request.headers.get("authorization")
   if auth_header and auth_header.startswith("Bearer "):
-      token = auth_header.removeprefix("Bearer ").strip()
-      try:
-          decoded_token = firebase_auth.verify_id_token(token)
-          user_id = decoded_token.get("uid", "unknown")
-      except InvalidIdTokenError:
-          user_id = "invalid-token"
-      except Exception as e:
-          user_id = f"error:{e.__class__.__name__}"
+    token = auth_header.removeprefix("Bearer ").strip()
+    try:
+      decoded_token = firebase_auth.verify_id_token(token)
+      user_id = decoded_token.get("uid", "unknown")
+    except InvalidIdTokenError:
+      user_id = "invalid-token"
+    except Exception as e:
+      user_id = f"error:{e.__class__.__name__}"
 
   logger.info(f"User ID: {user_id}")
 
@@ -54,6 +58,7 @@ async def log_reqeust(request: Request, call_next):
     )
   logger.info(f"Response: {response.status_code}")
   return response
+
 
 app.include_router(map.router, prefix="/api/v1/maps", tags=["Maps"])
 app.include_router(user.router, prefix="/api/v1/user", tags=["User"])
