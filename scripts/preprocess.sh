dd3c44574025edf70220c2a7a9a6b40f86fc4691
Zuhanit <zuhanit3@gmail.com>
2025-04-30 20:18:11 +0900

    fix: use relative path

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")"
 cd ../preprocess
 poetry run python -m terrain.main
-poetry run python -m graphics.main --path /Volumes/External/Programming/webditor/preprocess/graphics/anim --output /Volumes/External/Programming/webditor/preprocess/output/anim --sd --hd
+poetry run python -m graphics.main --path ./graphics/anim --output ../backend/static/anim --sd --hd
