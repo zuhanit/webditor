6c634ab3ad39eff7127330ba0fec06211dd96381
Zuhanit <zuhanit3@gmail.com>
2025-05-06 15:24:37 +0900

    feat: add image schema

--- frontend/types/SCImage.ts
+++ frontend/types/SCImage.ts
@@ -1,16 +1,19 @@
 import { z } from "zod";
 
+export const ImageVersionSchema = z.enum(["sd", "hd"]);
+export type ImageVersion = z.infer<typeof ImageVersionSchema>;
+
 export const SCImageParseSchema = z.object({
-  version: z.enum(["sd", "hd"]),
+  version: ImageVersionSchema,
   imageIndex: z.number().gte(0).lte(998),
 });
 
-export type SCImageType = z.infer<typeof SCImageParseSchema>;
+export type SCImage = z.infer<typeof SCImageParseSchema>;
 
 export interface SCImageBundle {
-  diffuse?: Blob;
+  diffuse: Blob;
   teamColor?: Blob;
-  meta?: FrameMeta;
+  meta: FrameMeta;
 }
 
 export interface FrameRect {
@@ -23,5 +26,7 @@ export interface FrameRect {
   unknown1: number;
   unknown2: number;
 }
+
+export type SCImageWithFrame = SCImage & { frame: number };
 
 export type FrameMeta = Record<number, FrameRect>;
