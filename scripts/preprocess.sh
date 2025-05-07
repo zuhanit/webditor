742c83563d67afaad15c3ef7f17b82801b815496
Zuhanit <zuhanit3@gmail.com>
2025-05-07 18:50:34 +0900

    feat: merge shell script to python script

--- scripts/preprocess.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-cd ../preprocess
-poetry run python -m terrain.main
-poetry run python -m graphics.main --path ./graphics/anim --output ../backend/static/anim --sd --hd
