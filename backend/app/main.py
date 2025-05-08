0df847650e0c7a450fc1e0e26dda72ca1bb70f34
Zuhanit <zuhanit3@gmail.com>
2025-05-08 17:53:11 +0900

    fix: add origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["*"],
+  allow_origins=["https://www.webditor.net"],  # Change origin if domain changed
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
