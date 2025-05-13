d931aad514bfa523405859536e4d668eb1f47b76
Zuhanit <zuhanit3@gmail.com>
2025-05-13 16:43:29 +0900

    fix: functional handling login

--- frontend/app/auth/signin/page.tsx
+++ frontend/app/auth/signin/page.tsx
@@ -1,35 +1,43 @@
 "use client";
 
+import api from "@/lib/api";
 import { getUserIdToken } from "@/lib/firebase/auth";
 import { auth } from "@/lib/firebase/clientApp";
-import axios from "axios";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLoginWithEmailAndPassword = async () => {
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      const token = await getUserIdToken();
-      const res = await axios.get("http://localhost:8000/api/v1/user/login", {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
+  const handleLoginWithEmailAndPassword = () =>
+    signInWithEmailAndPassword(auth, email, password)
+      .then(async () => {
+        const token = await getUserIdToken();
+        await api.get("/user/login", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      })
+      .catch((reason) => {
+        console.error("Failed to sign in:", reason);
       });
-    } catch (err: any) {
-      console.error("Failed to sign in:", err.message)
-    }
-  }
-  
+
   return (
     <div>
       <h1>Login</h1>
-      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={handleLoginWithEmailAndPassword}>Login</button>
     </div>
-  )  
+  );
 }
