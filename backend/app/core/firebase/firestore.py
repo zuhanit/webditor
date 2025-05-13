21d3624ca51f162a488db855fe5000377782bb9c
Zuhanit <zuhanit3@gmail.com>
2025-05-13 15:58:15 +0900

    chore: remove unused files

--- backend/app/core/firebase/firestore.py
+++
@@ -1,5 +0,0 @@
-from fastapi import File, UploadFile
-
-
-def upload_map(user: UploadFile,):
-  
--- backend/app/core/firebase/storage.py
+++
@@ -1,5 +0,0 @@
-from firebase_admin import storage, firestore 
-
-db = firestore.client()
-
-def add_map(content: st)
