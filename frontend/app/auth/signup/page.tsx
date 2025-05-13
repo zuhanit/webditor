0aa1cae36438ca80f724de26bdf0dcbe67847e7d
Zuhanit <zuhanit3@gmail.com>
2025-05-13 16:47:33 +0900

    refactor: functional handling signup

--- frontend/app/auth/signup/page.tsx
+++ frontend/app/auth/signup/page.tsx
@@ -1,36 +1,43 @@
 "use client";
 
+import api from "@/lib/api";
 import { getUserIdToken } from "@/lib/firebase/auth";
 import { auth } from "@/lib/firebase/clientApp";
-import axios from "axios";
-import { createUserWithEmailAndPassword } from "firebase/auth"
-import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 
 export default function Page() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
-  const signupWithEmail = async () => {
-    try {
-      const userCrendential = await createUserWithEmailAndPassword(auth, email, password)
-      const token = getUserIdToken();
 
-      const res = await axios.get("http://localhost:8000/api/v1/user/login", {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
+  const signupWithEmail = () =>
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async () => {
+        const token = await getUserIdToken();
+        api.get("/user/login", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
       })
-    } catch (err: any) {
-      console.error("Signup failed:", err.message);
-    }
-  }
+      .catch((reason) => {
+        console.error("Signup failed: ", reason.message);
+      });
 
   return (
     <div>
       <h1>Signup</h1>
-      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={signupWithEmail}>Sign Up</button>
     </div>
-  )
+  );
 }
