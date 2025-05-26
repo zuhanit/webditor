2d31439388af5a00d4ea6caef2b353a52d4651c0
Zuhanit <zuhanit3@gmail.com>
2025-05-26 17:59:35 +0900

    feat: add SearchForm

---
+++ frontend/components/form/search-form.tsx
@@ -0,0 +1,19 @@
+import { Search } from "lucide-react";
+import { SidebarGroup, SidebarGroupContent, SidebarInput } from "../ui/sidebar";
+import { Label } from "../ui/label";
+
+export function SearchForm({ ...props }: React.ComponentProps<"form">) {
+  return (
+    <form {...props}>
+      <SidebarGroup className="py-0">
+        <SidebarGroupContent className="relative">
+          <Label htmlFor="search" className="sr-only">
+            Search
+          </Label>
+          <SidebarInput id="search" placeholder="Search" className="pl-8" />
+          <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 opacity-50" />
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </form>
+  );
+}
