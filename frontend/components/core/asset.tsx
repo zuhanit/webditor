222ff193d6157d6a91b7ca57a39814c3c95db2de
Zuhanit <zuhanit3@gmail.com>
2025-05-29 14:54:56 +0900

    refactor: lowercase file name

--- frontend/components/core/asset.tsx
+++ frontend/components/core/asset.tsx
@@ -1,8 +1,35 @@
-import { useModals } from "@/hooks/useModals";
+import { useAssetStore } from "@/store/assetStore";
 import { useDraggable } from "@dnd-kit/core";
-import AssetEditor from "../asset_viewer/AssetEditor";
-import { Item } from "@/types/InspectorItem";
+import { Item } from "@/types/item";
 import { Card, CardHeader } from "../ui/card";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarInput,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+} from "../ui/sidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import { ChangeEvent, ReactNode, useEffect, useState } from "react";
+import { Plus, Minus } from "lucide-react";
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "../ui/collapsible";
+import { SCImageRenderer } from "./renderer";
+import { UnitSchema } from "@/types/schemas/Unit";
+import { useRawMapStore } from "@/store/mapStore";
+import { SpriteSchema } from "@/types/schemas/Sprite";
+import { ImageSchema } from "@/types/schemas/Image";
+import { WeaponDefinitionSchema } from "@/types/schemas/WeaponDefinition";
+import { Button } from "../ui/button";
+import { Editor } from "./editor";
 
 interface AssetCardProps {
   item: Item;
@@ -13,9 +40,13 @@ export function AssetCard({ item }: AssetCardProps) {
     id: item.label,
     data: item.properties,
   });
-  const { open } = useModals();
+  const { assets, setAssets, isEditorOpen, openEditor } = useAssetStore(
+    (state) => state
+  );
   const handleDoubleClick = () => {
-    open(AssetEditor, { label: item.label, item: item });
+    if (!isEditorOpen) openEditor();
+    if (assets.find((asset) => asset.label === item.label)) return;
+    setAssets([...assets, item]);
   };
 
   const style = transform
@@ -34,5 +65,110 @@ export function AssetCard({ item }: AssetCardProps) {
     >
       <CardHeader>{item.label}</CardHeader>
     </Card>
+  );
+}
+
+
+export function AssetEditorImage({ asset }: { asset: Item }) {
+  const [image, setImage] = useState<ReactNode | null>(null);
+  const usemap = useRawMapStore((state) => state.usemap);
+
+  const unitResult = UnitSchema.safeParse(asset.properties);
+  const spriteResult = SpriteSchema.safeParse(asset.properties);
+  const imageResult = ImageSchema.safeParse(asset.properties);
+  const weaponResult = WeaponDefinitionSchema.safeParse(asset.properties);
+
+  useEffect(() => {
+    console.log("gogi", unitResult, usemap, asset);
+    if (!usemap) return;
+
+    if (unitResult.success) {
+      const flingyID = unitResult.data.unit_definition.specification.graphics;
+      const spriteID = usemap.flingy[flingyID].sprite;
+      const imageID = usemap.sprite[spriteID].image;
+      setImage(<SCImageRenderer frame={0} version="sd" imageIndex={imageID} />);
+    }
+
+    if (imageResult.success) {
+      const i = imageResult.data.graphic;
+      setImage(<SCImageRenderer frame={0} version="sd" imageIndex={i} />);
+    }
+
+    if (spriteResult.success) {
+      const imageID = spriteResult.data.image;
+      setImage(<SCImageRenderer frame={0} version="sd" imageIndex={imageID} />);
+    }
+
+    if (weaponResult.success) {
+      const flingyID = weaponResult.data.graphics;
+      const spriteID = usemap.flingy[flingyID].sprite;
+      const imageID = usemap.sprite[spriteID].image;
+      setImage(<SCImageRenderer frame={0} version="sd" imageIndex={imageID} />);
+    }
+  }, [unitResult.success, usemap]);
+
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      {image}
+    </div>
+  );
+}
+
+export function AssetEditor() {
+  const { assets, setAssets, isEditorOpen, closeEditor } = useAssetStore();
+  const { updateRawMap } = useRawMapStore((state) => state);
+
+  if (!assets.length || !isEditorOpen) return null;
+
+  // FIXME: Using ID, or something else to identify asset
+  const onClickClose = (label: string) => {
+    const f = assets.filter((asset) => asset.label !== label);
+    setAssets(f);
+  };
+  const onClickSave = (asset: Item) => {
+    console.log(asset);
+    updateRawMap((draft: any) => {
+      let target = draft;
+      for (let i = 0; i < asset.path.length - 1; i++) {
+        target = target[asset.path[i]];
+      }
+      target[asset.path[asset.path.length - 1]] = asset.properties;
+    });
+  };
+  const defaultValue = assets[0].label;
+
+  return (
+    <div className="absolute flex bg-background-primary shadow-md">
+      <Sidebar>
+        <Tabs defaultValue={defaultValue}>
+          <SidebarHeader>
+            <TabsList>
+              {assets.map((asset) => (
+                <div key={asset.label} className="flex items-center gap-2">
+                  <TabsTrigger value={asset.label}>{asset.label}</TabsTrigger>
+                  <button onClick={() => onClickClose(asset.label)}>X</button>
+                </div>
+              ))}
+            </TabsList>
+          </SidebarHeader>
+          <SidebarContent>
+            <SidebarGroup>
+              {assets.map((asset) => (
+                <TabsContent
+                  className="flex"
+                  key={asset.label}
+                  value={asset.label}
+                >
+                  <Editor item={asset} />
+                  <AssetEditorImage asset={asset} />
+                  <Button onClick={() => onClickSave(asset)}>Save</Button>
+                  <Button onClick={closeEditor}>Close</Button>
+                </TabsContent>
+              ))}
+            </SidebarGroup>
+          </SidebarContent>
+        </Tabs>
+      </Sidebar>
+    </div>
   );
 }
--- frontend/types/Asset.ts
+++
@@ -1,4 +0,0 @@
-import { Item } from "./InspectorItem";
-
-export type AssetType = { id: number; item: Item };
-export type AssetResult = Record<string, AssetType[]>;
