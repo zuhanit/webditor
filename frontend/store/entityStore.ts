900aa1e4845e8e30254bc7253957db6681d728a6
Zuhanit <zuhanit3@gmail.com>
2025-05-30 14:56:19 +0900

    feat: add entity check feature

--- frontend/store/entityStore.ts
+++ frontend/store/entityStore.ts
@@ -4,11 +4,17 @@ import { create } from "zustand";
 interface EntityStore {
   entity: Entity | null;
   setEntity: (entity: Entity) => void;
+  checkedEntities: Entity[]
+  setCheckedEntities: (entities: Entity[]) => void;
 }
 
 export const useEntityStore = create<EntityStore>((set) => ({
   entity: null,
+  checkedEntities: [],
   setEntity: (entity) => {
     set({ entity });
+  },
+  setCheckedEntities: (entities) => {
+    set({ checkedEntities: entities });
   },
 }));
