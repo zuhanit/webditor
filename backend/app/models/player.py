a6fffbe8e13ad33efd42fc93b5ac53ed1d7c9b77
Zuhanit <zuhanit3@gmail.com>
2025-06-02 23:27:33 +0900

    refactor: move static type definition to external module

--- backend/app/models/player.py
+++ backend/app/models/player.py
@@ -1,68 +1,14 @@
 from enum import Flag
 from pydantic import Field
-from typing import TypeAlias, Literal
 from .wobject import WObject
-
-PlayerType: TypeAlias = Literal[
-  "Inactive",
-  "Computer (game)",
-  "Occupied By Human Player",
-  "Rescue",
-  "Unused",
-  "Computer",
-  "Human (Open Slot)",
-  "Neutral",
-  "Closed Slot",
-]
-
-OwnrPlayerTypeDict: dict[int, PlayerType] = {
-  0: "Inactive",
-  1: "Computer (game)",
-  2: "Occupied By Human Player",
-  3: "Rescue",
-  4: "Unused",
-  5: "Computer",
-  6: "Human (Open Slot)",
-  7: "Neutral",
-  8: "Closed Slot",
-}
-
-OwnrPlayerTypeReverseDict: dict[PlayerType, int] = {
-  v: k for k, v in OwnrPlayerTypeDict.items()
-}
-
-PlayerRace: TypeAlias = Literal[
-  "Zerg",
-  "Terran",
-  "Protoss",
-  "Invalid (Independant)",
-  "Invalid (Neutral)",
-  "User Selectable",
-  "Random",
-  "Inactive",
-]
-
-SidePlayerRaceDict: dict[int, PlayerRace] = {
-  0: "Zerg",
-  1: "Terran",
-  2: "Protoss",
-  3: "Invalid (Independant)",
-  4: "Invalid (Neutral)",
-  5: "User Selectable",
-  6: "Random",
-  7: "Inactive",
-}
-
-SidePlayerRaceReverseDict: dict[PlayerRace, int] = {
-  v: k for k, v in SidePlayerRaceDict.items()
-}
+from app.types.race import PlayerType, Race
 
 
 class Player(WObject):
   color: int
   rgb_color: tuple[int, int, int]
   player_type: PlayerType
-  race: PlayerRace
+  race: Race
   force: int = Field(default=0, lt=4, ge=0)
 
 
