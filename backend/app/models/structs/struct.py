a047d077ecfa15a4c9f47fb2d9a121061ce2a7c3
Zuhanit <zuhanit3@gmail.com>
2025-06-04 14:45:35 +0900

    refactor: remove unnecessary fields from Struct model

--- backend/app/models/structs/struct.py
+++ backend/app/models/structs/struct.py
@@ -1,6 +1,5 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class Struct(BaseModel):
-  current: int = Field(default=0, ge=0)
-  max: int = Field(default=0, ge=0)
+  pass
