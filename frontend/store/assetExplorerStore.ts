12abe1644b07f22ae736dfdd853f8d7025013eb6
Zuhanit <zuhanit3@gmail.com>
2025-06-15 22:55:57 +0900

    feat: use schema-based Asset

--- frontend/store/assetExplorerStore.ts
+++ frontend/store/assetExplorerStore.ts
@@ -2,8 +2,8 @@ import { create } from "zustand";
 import { Asset } from "@/types/schemas/asset/Asset";
 
 interface AssetExplorerStore {
-  currentAsset: Asset | null;
-  setCurrentAsset: (asset: Asset) => void;
+  currentAsset: Asset & { children?: Asset[] } | null;
+  setCurrentAsset: (asset: Asset & { children?: Asset[] }) => void;
 }
 
 export const useAssetExplorerStore = create<AssetExplorerStore>((set) => ({
