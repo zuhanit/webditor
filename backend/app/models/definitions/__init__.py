7dfaa50e540d6783fee1f320f146ab0bfa15cb6e
Zuhanit <zuhanit3@gmail.com>
2025-06-16 00:28:50 +0900

    feat: definition import

---
+++ backend/app/models/definitions/__init__.py
@@ -0,0 +1,16 @@
+from .definition import Definition
+from .flingy import FlingyDefinition
+from .image import ImageDefinition
+from .order import OrderDefinition
+from .portrait import PortraitDefinition
+from .sprite import SpriteDefinition
+from .tech import (
+  Technology,
+  TechRestriction,
+  Upgrade,
+  UpgradeRestriction,
+  UpgradeSetting,
+  TechRestriction,
+)
+from .unit import UnitDefinition, UnitRestriction
+from .weapon import WeaponDefinition
