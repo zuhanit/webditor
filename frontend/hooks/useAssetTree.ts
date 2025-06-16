1322307ee336312bfc85b669f00554eb3d992dbb
Zuhanit <zuhanit3@gmail.com>
2025-06-16 18:13:14 +0900

    refactor: add children property

--- frontend/hooks/useAssetTree.ts
+++ frontend/hooks/useAssetTree.ts
@@ -1,16 +1,16 @@
 import { Asset } from "@/types/schemas/asset/Asset";
 import { useMemo } from "react";
 
-export function useEntityAssetTree(totalEntities: Asset[]) {
+export function useEntityAssetTree(totalEntities: Asset[]): (Asset & { children?: Asset[] })[] {
   const tree = useMemo(() => {
-    const childrenMap = new Map<number | null, Asset[]>();
+    const childrenMap = new Map<number | null, Asset[] & { children?: Asset[] }>();
     totalEntities.forEach(entity => {
       const key = entity.parent_id ?? -1;
       if (!childrenMap.has(key)) childrenMap.set(key, []);
       childrenMap.get(key)!.push(entity);
     });
 
-    const buildTree = (parentId: number | null = -1): Asset[] => {
+    const buildTree = (parentId: number | null = -1): Asset[] & { children?: Asset[] }[] => {
       return (childrenMap.get(parentId) || []).map((entity) => {
         return {
           ...entity,
@@ -25,16 +25,16 @@ export function useEntityAssetTree(totalEntities: Asset[]) {
 
 }
 
-export function useAssetTree(totalAssets: Asset[]) {
+export function useAssetTree(totalAssets: Asset[]): (Asset & { children?: Asset[] })[] {
   return useMemo(() => {
-    const childrenMap = new Map<number | null, Asset[]>();
+    const childrenMap = new Map<number | null, Asset[] & { children?: Asset[] }>();
     totalAssets.forEach(asset => {
       const key = asset.parent_id ?? -1;
       if (!childrenMap.has(key)) childrenMap.set(key, []);
       childrenMap.get(key)!.push(asset);
     });
 
-    const buildTree = (parentId: number | null = -1): Asset[] => {
+    const buildTree = (parentId: number | null = -1): Asset[] & { children?: Asset[] }[] => {
       return (childrenMap.get(parentId) || []).map(asset => ({
         ...asset,
         children: buildTree(asset.id),
